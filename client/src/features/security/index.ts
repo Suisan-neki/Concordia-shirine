94fff6404c47f9d5716638565e9369786645724f
Suisan-neki <b241651@hiroshima-u.ac.jp>
2026-01-20 09:55:13 +0900

    セキュリティ機能の集約エントリを追加 #35

---
+++ client/src/features/security/index.ts
@@ -0,0 +1,3 @@
+export { SecurityBarrier } from '@/components/SecurityBarrier';
+export { SecurityDashboard } from '@/components/SecurityDashboard';
+export { SecurityDetailPanel } from '@/components/SecurityDetailPanel';
